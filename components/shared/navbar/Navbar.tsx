ad13a82f2c3e779fdfacf9a7d772ae36dbc15f60
Sajgroundhogday <sajgroundhogday@gmail.com>
2024-10-23 21:42:12 +0530

    modified the themeprovider to handle themes according to system settings

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import { SignedIn, UserButton } from '@clerk/nextjs'
+import Theme from './Theme'
+
 const Navbar = () => {
   return (
     <nav className='flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12'>
@@ -13,7 +15,7 @@ const Navbar = () => {
         GlobalSearch
 
         <div className='flex-between gap-5'>
-            Theme
+            <Theme/>
             <SignedIn>
                 <UserButton 
                   afterSwitchSessionUrl='/'
